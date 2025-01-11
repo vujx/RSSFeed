9f668eb6ae34a001c0d70bad4e4c7c1089ed12ab
tomislavvujevic <tomislav.vujevic@undabot.com>
2025-01-11 17:21:40 +0100

    Remove unnecessary content negotiation and add header, content type in request

--- data/src/main/java/com/rssfeed/data/api/ApiServiceImpl.kt
+++ data/src/main/java/com/rssfeed/data/api/ApiServiceImpl.kt
@@ -2,8 +2,13 @@ package com.rssfeed.data.api
 
 import com.rssfeed.data.api.model.RssFeed
 import io.ktor.client.HttpClient
+import io.ktor.client.request.header
 import io.ktor.client.request.request
 import io.ktor.client.statement.bodyAsText
+import io.ktor.client.utils.EmptyContent.contentType
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.contentType
 import kotlinx.serialization.decodeFromString
 import nl.adaptivity.xmlutil.serialization.XML
 
@@ -14,7 +19,10 @@ class ApiServiceImpl(
 
   override suspend fun addRssFeed(url: String): RssFeed {
     val response = client
-      .request(url)
+      .request(url) {
+        header(HttpHeaders.Accept, "application/xml")
+        contentType(ContentType.Application.Xml)
+      }
       .bodyAsText()
 
     return xml.decodeFromString<RssFeed>(response)
