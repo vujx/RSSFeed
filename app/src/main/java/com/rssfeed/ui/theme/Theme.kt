a26a121398368dd021e27465549cc0852b918d1f
tomislavvujevic <tomislav.vujevic@undabot.com>
2025-01-12 17:13:17 +0100

    Add theming and styles

--- app/src/main/java/com/rssfeed/ui/theme/Theme.kt
+++ app/src/main/java/com/rssfeed/ui/theme/Theme.kt
@@ -1,47 +1,24 @@
 package com.rssfeed.ui.theme
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme = darkColorScheme(
-  primary = Purple80,
-  secondary = PurpleGrey80,
-  tertiary = Pink80,
-)
-
-private val LightColorScheme = lightColorScheme(
-  primary = Purple40,
-  secondary = PurpleGrey40,
-  tertiary = Pink40,
+private val LightColorPalette = lightColors(
+  primary = Color.DarkGray,
+  primaryVariant = Color.LightGray,
+  secondary = Color.Gray,
+  onPrimary = Color.White,
+  onSurface = Color.Black,
 )
 
 @Composable
-fun RSSFeedTheme(
-  darkTheme: Boolean = isSystemInDarkTheme(),
-  dynamicColor: Boolean = true,
-  content: @Composable () -> Unit,
-) {
-  val colorScheme =
-    when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-        val context = LocalContext.current
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-
-      darkTheme -> DarkColorScheme
-      else -> LightColorScheme
-    }
-
+fun RSSFeedTheme(content: @Composable () -> Unit) {
   MaterialTheme(
-    colorScheme = colorScheme,
+    colors = LightColorPalette,
     typography = Typography,
+    shapes = Shapes,
     content = content,
   )
 }
--- app/src/main/java/com/rssfeed/ui/theme/Type.kt
+++ app/src/main/java/com/rssfeed/ui/theme/Type.kt
@@ -1,17 +1,33 @@
 package com.rssfeed.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 val Typography = Typography(
-  bodyLarge = TextStyle(
-    fontFamily = FontFamily.Default,
+  body1 = TextStyle(
     fontWeight = FontWeight.Normal,
     fontSize = 16.sp,
-    lineHeight = 24.sp,
-    letterSpacing = 0.5.sp,
+  ),
+  body2 = TextStyle(
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp,
+  ),
+  h1 = TextStyle(
+    fontWeight = FontWeight.Bold,
+    fontSize = 12.sp,
+  ),
+  h2 = TextStyle(
+    fontWeight = FontWeight.Bold,
+    fontSize = 16.sp,
+  ),
+  h3 = TextStyle(
+    fontWeight = FontWeight.Bold,
+    fontSize = 20.sp,
+  ),
+  h4 = TextStyle(
+    fontWeight = FontWeight.Bold,
+    fontSize = 24.sp,
   ),
 )
