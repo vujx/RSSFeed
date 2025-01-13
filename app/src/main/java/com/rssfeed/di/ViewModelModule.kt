95ca5da81a1e7c9fc173c98e8b864e1b8464e056
tomislavvujevic <tomislav.vujevic@undabot.com>
2025-01-13 21:28:43 +0100

    Add favorites view model

--- app/src/main/java/com/rssfeed/di/ViewModelModule.kt
+++ app/src/main/java/com/rssfeed/di/ViewModelModule.kt
@@ -1,9 +1,11 @@
 package com.rssfeed.di
 
+import com.rssfeed.feature.favorites.viewmodel.FavoritesViewModel
 import com.rssfeed.feature.home.viewmodel.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
   viewModelOf(::HomeViewModel)
+  viewModelOf(::FavoritesViewModel)
 }
