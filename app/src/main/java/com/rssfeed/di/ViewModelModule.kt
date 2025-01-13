672ebf688cb51ba802d8909a8331c2d224c60faa
tomislavvujevic <tomislav.vujevic@undabot.com>
2025-01-13 21:53:58 +0100

    Add articles view model

--- app/src/main/java/com/rssfeed/di/ViewModelModule.kt
+++ app/src/main/java/com/rssfeed/di/ViewModelModule.kt
@@ -1,5 +1,6 @@
 package com.rssfeed.di
 
+import com.rssfeed.feature.articles.viewmodel.ArticlesViewModel
 import com.rssfeed.feature.favorites.viewmodel.FavoritesViewModel
 import com.rssfeed.feature.home.viewmodel.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
@@ -8,4 +9,5 @@ import org.koin.dsl.module
 val viewModelModule = module {
   viewModelOf(::HomeViewModel)
   viewModelOf(::FavoritesViewModel)
+  viewModelOf(::ArticlesViewModel)
 }
