a978ebe20d17648f259aed71c255ebd934a2e2f1
tomislavvujevic <tomislav.vujevic@undabot.com>
2025-01-14 13:02:20 +0100

    Fix rss feed url validation

--- domain/src/main/java/com/rssfeed/domain/validator/RssFeedUrlValidator.kt
+++ domain/src/main/java/com/rssfeed/domain/validator/RssFeedUrlValidator.kt
@@ -5,13 +5,8 @@ class RssFeedUrlValidator {
   private companion object {
     const val HTTP_PREFIX = "http://"
     const val HTTPS_PREFIX = "https://"
-    val DEFAULT_RSS_INDICATORS = listOf(".rss", ".xml", "/feed", "/rss", "?rss", "?feed")
   }
 
-  operator fun invoke(url: String): Boolean {
-    val hasValidProtocol = url.startsWith(HTTP_PREFIX) || url.startsWith(HTTPS_PREFIX)
-    val hasRssIndicator = DEFAULT_RSS_INDICATORS.any { url.contains(it) }
-
-    return hasValidProtocol && hasRssIndicator
-  }
+  operator fun invoke(url: String): Boolean =
+    url.startsWith(HTTP_PREFIX) || url.startsWith(HTTPS_PREFIX)
 }
