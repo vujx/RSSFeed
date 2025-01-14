a8dc463325d835d6912e65821b8c70f48c45d6b3
tomislavvujevic <tomislav.vujevic@undabot.com>
2025-01-14 17:09:12 +0100

    Add show notification usecase

--- domain/src/main/java/com/rssfeed/domain/di/UseCaseModule.kt
+++ domain/src/main/java/com/rssfeed/domain/di/UseCaseModule.kt
@@ -2,10 +2,12 @@ package com.rssfeed.domain.di
 
 import com.rssfeed.domain.usecase.AddRssFeed
 import com.rssfeed.domain.usecase.DeleteChannel
+import com.rssfeed.domain.usecase.IsNotificationPermissionGranted
 import com.rssfeed.domain.usecase.ObserveArticles
 import com.rssfeed.domain.usecase.ObserveChannels
 import com.rssfeed.domain.usecase.ObserveFavoriteChannels
-import com.rssfeed.domain.usecase.SyncAndGetUpdatedSubscribedChannelTitles
+import com.rssfeed.domain.usecase.ShowNotification
+import com.rssfeed.domain.usecase.SyncAndGetUpdatedSubscribedChannels
 import com.rssfeed.domain.usecase.ToggleFavoriteChannel
 import com.rssfeed.domain.usecase.ToggleSubscribedChannel
 import org.koin.core.module.dsl.factoryOf
@@ -14,10 +16,12 @@ import org.koin.dsl.module
 val useCaseModule = module {
   factoryOf(::AddRssFeed)
   factoryOf(::DeleteChannel)
+  factoryOf(::IsNotificationPermissionGranted)
+  factoryOf(::ShowNotification)
   factoryOf(::ObserveArticles)
   factoryOf(::ObserveChannels)
   factoryOf(::ObserveFavoriteChannels)
   factoryOf(::ToggleFavoriteChannel)
   factoryOf(::ToggleSubscribedChannel)
-  factoryOf(::SyncAndGetUpdatedSubscribedChannelTitles)
+  factoryOf(::SyncAndGetUpdatedSubscribedChannels)
 }
---
+++ domain/src/main/java/com/rssfeed/domain/usecase/ShowNotification.kt
@@ -0,0 +1,67 @@
+package com.rssfeed.domain.usecase
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.core.app.NotificationCompat
+import androidx.core.app.TaskStackBuilder
+import com.rssfeed.domain.R
+
+class ShowNotification(
+  private val context: Context,
+) {
+
+  operator fun invoke(
+    title: String,
+    body: String,
+    uri: String,
+  ) {
+    val notificationBuilder = createNotificationBuilder(
+      title = title,
+      body = body,
+      uri = uri,
+    )
+    createNotificationManager(notificationBuilder)
+  }
+
+  private fun createNotificationBuilder(
+    title: String,
+    body: String,
+    uri: String,
+  ): NotificationCompat.Builder {
+    return NotificationCompat.Builder(context, CHANNEL_ID)
+      .setContentTitle(title)
+      .setContentText(body)
+      .setSmallIcon(R.drawable.ic_notification)
+      .setContentIntent(getContentIntent(uri))
+      .setAutoCancel(true)
+  }
+
+  private fun getContentIntent(uri: String): PendingIntent? {
+    val intent = Intent().apply {
+      action = Intent.ACTION_VIEW
+      data = Uri.parse(uri)
+    }
+
+    return TaskStackBuilder.create(context).run {
+      addNextIntentWithParentStack(intent)
+      getPendingIntent(REQUEST_ID, immutableFlags())
+    }
+  }
+
+  private fun immutableFlags() = PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+
+  private fun createNotificationManager(
+    notificationBuilder: NotificationCompat.Builder,
+  ) {
+    val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+  }
+}
+
+const val CHANNEL_ID = "contentFeedId"
+const val CHANNEL_NAME = "ContentFeedNotifications"
+const val REQUEST_ID = 1234
+const val NOTIFICATION_ID = 1
