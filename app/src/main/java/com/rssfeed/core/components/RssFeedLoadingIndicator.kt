71e1ef22b87fc6d04d399586761e28df217bb9df
tomislavvujevic <tomislav.vujevic@undabot.com>
2025-01-15 15:11:53 +0100

    Remove loading indicator and take over clicks when content is loading

--- app/src/main/java/com/rssfeed/core/components/RssFeedLoadingIndicator.kt
+++ app/src/main/java/com/rssfeed/core/components/RssFeedLoadingIndicator.kt
@@ -1,10 +1,13 @@
 package com.rssfeed.core.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
@@ -13,7 +16,12 @@ fun LoadingIndicator(
   modifier: Modifier = Modifier,
 ) {
   Box(
-    modifier = modifier.fillMaxSize(),
+    modifier = modifier
+      .fillMaxSize()
+      .clickable(
+        interactionSource = remember { MutableInteractionSource() },
+        indication = null,
+      ) { },
     contentAlignment = Alignment.Center,
   ) {
     CircularProgressIndicator(color = MaterialTheme.colors.secondary)
